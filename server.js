874bf84c0ce354e98af9a4abd73a8585434b4ee4
Quinn <ducquynhnguyeninfo@gmail.com>
2021-03-24 20:29:27 +0700

    fix the deploy command

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
@@ -6,3 +8,7 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
